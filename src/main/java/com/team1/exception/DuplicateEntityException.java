28c1983c6ea959871495d68737ca3e2519342223
aleschopf <alexschopf05@gmail.com>
2024-09-21 16:10:12 -0300

    docs: add description to DuplicateEntity and EntityNotFound exceptions

--- src/main/java/com/team1/exception/DuplicateEntityException.java
+++ src/main/java/com/team1/exception/DuplicateEntityException.java
@@ -1,6 +1,19 @@
 package main.java.com.team1.exception;
 
+/**
+ * Exceção personalizada lançada quando ocorre uma tentativa de duplicar
+ * uma entidade em operações de cadastro ou atualização.
+ * <p>
+ * Esta exceção é utilizada para garantir que entidades únicas, como veículos,
+ * clientes ou agências, não sejam cadastradas mais de uma vez no sistema.
+ * </p>
+ */
 public class DuplicateEntityException extends Exception {
+    /**
+     * Construtor para a exceção DuplicateEntityException.
+     *
+     * @param message A mensagem descritiva que detalha a causa da exceção.
+     */
     public DuplicateEntityException(String message) {
         super(message);
     }
--- src/main/java/com/team1/exception/EntityNotFoundException.java
+++ src/main/java/com/team1/exception/EntityNotFoundException.java
@@ -1,6 +1,20 @@
 package main.java.com.team1.exception;
 
+/**
+ * Exceção personalizada lançada quando uma entidade não é encontrada
+ * durante operações de busca ou manipulação no sistema.
+ * <p>
+ * Esta exceção é utilizada para sinalizar que uma determinada entidade,
+ * como veículos, clientes ou agências, não está presente no sistema.
+ * </p>
+ */
 public class EntityNotFoundException extends Exception {
+
+    /**
+     * Construtor para a exceção EntityNotFoundException.
+     *
+     * @param message A mensagem descritiva que detalha a causa da exceção.
+     */
     public EntityNotFoundException(String message) {
         super(message);
     }
