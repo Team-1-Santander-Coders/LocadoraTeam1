cbd515b9e4034453e3f84afc4ec869d3e91342aa
Matheus Lima Moreira <102155883+mathlimam@users.noreply.github.com>
2024-09-22 16:00:10 -0300

    func: Added Customer, LegalPerson and PhysicalPerson entities

--- src/main/java/com/team1/entities/Customer.java
+++ src/main/java/com/team1/entities/Customer.java
@@ -1,4 +1,24 @@
 package main.java.com.team1.entities;
 
-public abstract class Customer {
+import java.io.Serializable;
+
+public interface Customer extends Serializable {
+
+    public String getName();
+
+    public void setName(String name);
+
+    public String getAddress();
+
+    public void setAddress(String address);
+
+    public String getPhone();
+
+    public void setPhone(String phone);
+
+    public boolean equals(Customer customer);
+
+    public int hashCode();
+
+    public String toString();
 }
