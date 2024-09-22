8d2674c4b2f5b57db5b777e9fffe96d58537a6fe
Matheus Lima Moreira <102155883+mathlimam@users.noreply.github.com>
2024-09-22 16:02:56 -0300

    func: Added DTO Customers classes
    Added CustomerDTO abstract class, LegalPersonDTO and PhysicalPersonDTO

--- src/main/java/com/team1/dto/CustomerDTO.java
+++ src/main/java/com/team1/dto/CustomerDTO.java
@@ -1,4 +1,116 @@
 package main.java.com.team1.dto;
 
-public record CustomerDTO() {
+import java.io.Serial;
+import java.io.Serializable;
+
+public abstract class CustomerDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    /**
+     * A CustomerDTO é uma classe abstrata, que visa definir a estrutura que as classes filhas precisam seguir. Também
+     * define os métodos básicos, como os Getters e Setters. Essa classe também implementa a interfacde Serializable,
+     * necessária para a serialização dos objetos pela classe <code>FileUtil</code>, garantindo a persistência de dados.
+     */
+    private String name;
+    private String address;
+    private String phone;
+
+    /**
+     * Construtor da classe abstrata, que será acessada via <code>super()</code> pelas classes filhas
+     * @param name
+     * @param address
+     * @param phone
+     */
+    protected CustomerDTO(String name, String address, String phone){
+
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    /**
+     * Método de acesso da String privada Name
+     * @return String Name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Método para definir o valor da String privada Name
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Método de acesso à String privada Address
+     * @return String Address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Método para definir o valor da String privada Address
+     * @param address
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Método de acesso à String privada Phone
+     * @return String Phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * Método para definir o valor da String privada Phone;
+     * @param phone
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Método que retorna a classe da instancia do objeto
+     * @return String class SimpleName
+     */
+    public String typeOf(){
+        return this.getClass().getSimpleName();
+    };
+
+    /**
+     * Método sobrescrito, que garante uma melhor visualização dos objetos dessa classe
+     * @return String toString
+     */
+    @Override
+    public String toString() {
+        return "[ " + typeOf() + " ]" +
+                " name= " + name + ", " +
+                "address= " + address + ", " +
+                "phone= " + phone + "]";
+    }
+
+    /**
+     * Sobrescrita do método equals. Seguindo as regras de negócio,
+     * caso dois customersDTO genéricos possuam o mesmo nome, eles são iguais.
+     * @param customerDTO
+     * @return boolean customerDTO.name.equals(this.name);
+     */
+    public boolean equals(CustomerDTO customerDTO){
+        return customerDTO.name.equals(this.name);
+    }
+
+    /**
+     * Sobrescrita do método hashCode para garantir a lógica de que, se dois objetos da classe possuem o mesmo nome, eles são iguais
+     * @return
+     */
+    public int hashCode(){
+        return (31 * name.hashCode() + name.hashCode());
+    }
 }
--- src/main/java/com/team1/dto/PhysicalPersonDTO.java
+++ src/main/java/com/team1/dto/PhysicalPersonDTO.java
@@ -1,4 +1,81 @@
 package main.java.com.team1.dto;
 
-public record PhysicalPersonDTO() {
+import java.io.Serial;
+
+/**
+ * A classe <code>LegalPersonDTO</code> é uma subclasse da classe abstrata CustomerDTO,
+ * herdando todos as suas caracteristicas, sendo necessária a implementação dos métodos.
+ * Como a classe pai é Serializavel, essa subclasse mantem essa caracteristica.
+ */
+
+public class PhysicalPersonDTO extends CustomerDTO {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String cpf;
+
+    /**
+     * No construtor da <code>PhysicalPersonDTO</code>, temos a inclusão de mais um parametro: O CPF.
+
+     * @param name
+     * @param address
+     * @param phone
+     * @param cpf
+     */
+    public PhysicalPersonDTO(String name, String address, String phone, String cpf)  {
+        super(name, address, phone);
+        this.cpf = cpf;
+    }
+
+    /**
+     * Método de acesso à String privada cpf
+     * @return String cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * Método utilizado para definir o valor da String privada cpf
+     * @param cpf
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * Sobrescrita do método equals da classe pai, definindo melhor o critério de igualdade entre dois objetos.
+     * Se dois objetos do tipo PhysicalPersonDto possuem o mesmo CPF, elçes são iguais
+     * @param customerDTO
+     * @return ((PhysicalPersonDTO) customerDTO).getCpf().equals(this.getCpf())
+     */
+    @Override
+    public boolean equals(CustomerDTO customerDTO) {
+        if(customerDTO instanceof PhysicalPersonDTO) {
+            return ((PhysicalPersonDTO) customerDTO).getCpf().equals(this.getCpf());
+        }
+        return false;
+    }
+
+    /**
+     * Como houve a sobrescrita do método equals, definimos também uma nova lógica para o HashCode, garantindo que,
+     * caso dois objetos da classe PhysicalPersonDTO possuam o mesmo cpf, ambos terão o mesmo hashCode
+     * @return (31*this.cpf.hashCode()) + this.cpf.hashCode();
+     */
+    public int hashCode(){
+        return (31*this.cpf.hashCode()) + this.cpf.hashCode();
+    }
+
+    /**
+     * Sobrescrita do método toString, para garantir que a informação do CPF seja visualizada
+     * @return String toString
+     */
+    @Override
+    public String toString() {
+        return "[ " + typeOf() + " ] " +
+                "name= " + getName() + ", " +
+                "address= " + getAddress() + ", " +
+                "phone= " + getPhone() + ", " +
+                "cpf= " + getCpf()  + "]";
+    }
+
 }
