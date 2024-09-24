31dfee3b3ce1e3bc51999ab07946a9797dd8a9d0
Matheus Lima Moreira <102155883+mathlimam@users.noreply.github.com>
2024-09-23 23:17:58 -0300

    updated CustomerRepositoryImpl

--- src/main/java/com/team1/repository/CustomerRepositoryImpl.java
+++ src/main/java/com/team1/repository/CustomerRepositoryImpl.java
@@ -148,6 +148,7 @@ public class CustomerRepositoryImpl implements CustomerRepository {
      */
     private boolean isIncluded(CustomerDTO customerDTO) {
         List<CustomerDTO> customersList = findAll();
+        if(customersList.getFirst() == null) return false;
         for(CustomerDTO customer : customersList){
                 if(customerDTO.hashCode() == customer.hashCode())
                     return true;
