e41ea6e7867eafe9cb41b3c3473055d7f9af8ee7
Matheus Lima Moreira <102155883+mathlimam@users.noreply.github.com>
2024-09-23 23:19:43 -0300

    added USER route index.html and script.js

---
+++ src/resources/User/script.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById('userForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const address = document.getElementById('address').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const documentValue = document.getElementById('documentValue').value;
+    const tipo = document.getElementById('tipo').value;
+
+    fetch('/user', {
+        method: 'POST',
+        body: `${name},${address},${password},${email}, ${phone}, ${documentValue}, ${tipo}`
+
+    }).then(response => {
+        if (response.ok) {
+            alert('Usuario cadastrado com sucesso!');
+            document.getElementById('userForm').reset()
+        } else {
+            alert(response.statusText);
+        }
+    });
+});
